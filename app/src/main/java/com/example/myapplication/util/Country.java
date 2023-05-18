3d2960907f1cb8836ae963e6c3e09b323c68f52a
Anna Shimizu <ashi725@aucklanduni.ac.nz>
2023-05-18 17:53:49 +1200

    Implemented country class

--- app/src/main/java/com/example/myapplication/util/Country.java
+++ app/src/main/java/com/example/myapplication/util/Country.java
@@ -1,4 +1,29 @@
 package com.example.myapplication.util;
 
+import java.util.List;
+
 public class Country {
+
+    private String name;
+    private List<String> images;
+    private String description;
+
+    // Constructor
+    public Country(String name, List<String> images, String description){
+        this.name = name;
+        this.images = images;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getImages() {
+        return images;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
