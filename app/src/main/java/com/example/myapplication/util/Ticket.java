68ac2df66bfe9dde0ebbf541d3dc082cfdf2ad76
Anna Shimizu <ashi725@aucklanduni.ac.nz>
2023-05-18 18:25:38 +1200

    Implemented Ticket class

--- app/src/main/java/com/example/myapplication/util/Ticket.java
+++ app/src/main/java/com/example/myapplication/util/Ticket.java
@@ -1,4 +1,29 @@
 package com.example.myapplication.util;
 
+import java.util.List;
+
 public class Ticket {
+
+    private String date;
+    private double price;
+    private Country destination;
+
+    // Constructor
+    public Ticket(String date, double price, Country destination){
+        this.date = date;
+        this.price = price;
+        this.destination = destination;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public Country getDestination() {
+        return destination;
+    }
 }
