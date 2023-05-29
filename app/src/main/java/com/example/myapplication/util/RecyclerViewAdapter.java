21a83bd5353c6aa4f6f136d9180f5ba2526b019b
dher577 <dher577@aucklanduni.ac.nz>
2023-05-29 19:44:28 +1200

    Recycler view reimplemented

--- app/src/main/java/com/example/myapplication/util/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/myapplication/util/RecyclerViewAdapter.java
@@ -1,25 +1,62 @@
 package com.example.myapplication.util;
 
 import androidx.recyclerview.widget.RecyclerView;
+import android.view.View;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import com.example.myapplication.R;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
+    private String[] localDataSet;
+
     // Provide a direct reference to each of the views within a data item
     // Used to cache the views within the item layout for fast access
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        // Your holder should contain a member variable
-        // for any view that will be set as you render a row
-        public TextView nameTextView;
-        public Button messageButton;
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView textView;
 
-        // We also create a constructor that accepts the entire item row
-        // and does the view lookups to find each subview
-        public ViewHolder(View itemView) {
-            // Stores the itemView in a public final member variable that can be used
-            // to access the context from any ViewHolder instance.
-            super(itemView);
-
-            nameTextView = (TextView) itemView.findViewById(R.id.contact_name);
-            messageButton = (Button) itemView.findViewById(R.id.message_button);
+        public ViewHolder(View view) {
+            super(view);
+            // Define click listener for the ViewHolder's View
+            textView = (TextView) view.findViewById(R.id.textView);
         }
+
+        public TextView getTextView() {
+            return textView;
+        }
+    }
+    /**
+     * Initialize the dataset of the Adapter
+     *
+     * @param dataSet String[] containing the data to populate views to be used
+     * by RecyclerView
+     */
+    public CustomAdapter(String[] dataSet) {
+        localDataSet = dataSet;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // Create a new view, which defines the UI of the list item
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.text_row_item, viewGroup, false);
+
+        return new ViewHolder(view);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.getTextView().setText(localDataSet[position]);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return localDataSet.length;
     }
 }
