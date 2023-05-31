f9b7a7a7564353e57e4d7616ccebc584152e1944
Anna Shimizu <ashi725@aucklanduni.ac.nz>
2023-06-01 00:54:40 +1200

    Linked details and date selection

--- app/src/main/java/com/example/myapplication/activities/DetailsActivity.java
+++ app/src/main/java/com/example/myapplication/activities/DetailsActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -40,6 +42,14 @@ public class DetailsActivity extends AppCompatActivity {
         countryTitle.setText(countryName);
         whyCountry.setText("Why " + countryName + "?");
         description.setText(countryDescription);
+
+        Button checkPricesButton = findViewById(R.id.pricesButton);
+        checkPricesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onCheckPrices();
+            }
+        });
     }
 
     public void onCheckPrices() {
