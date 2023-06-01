704f2ad824ca06e86a5345f423e193bbe82a26c1
dher577 <dher577@aucklanduni.ac.nz>
2023-06-01 14:00:03 +1200

    Bug fixes

--- app/src/main/java/com/example/myapplication/activities/ListActivity.java
+++ app/src/main/java/com/example/myapplication/activities/ListActivity.java
@@ -54,9 +54,6 @@ public class ListActivity extends AppCompatActivity {
         homeButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Intent listIntent = new Intent(getBaseContext(), MainActivity.class);
-                byContinent = 0;
-                startActivity(listIntent);
                 Intent homeIntent = new Intent(getBaseContext(), MainActivity.class);
                 byContinent = 0;
                 startActivity(homeIntent);
